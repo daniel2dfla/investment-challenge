86f7fd24c1b60bff70cd36ddd2a3ca567abcca6c
Daniel Nascimento <98188853+daniel2dfla@users.noreply.github.com>
2024-07-28 15:14:03 -0300

    feature: created the user entity and connected it to the Mysql database (#5)

---
+++ src/main/java/developer/backend/CRUD/entity/User.java
@@ -0,0 +1,40 @@
+package developer.backend.CRUD.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @CreationTimestamp
+    private Instant creationTimestamp;
+
+    @UpdateTimestamp
+    private Instant updateTimestamp;
+}
