141286a54619e459a88a3985aaff7b7f4840f83b
daniel2dfla <daniel_2dfla@hotmail.com>
2024-08-04 15:37:34 -0300

    feature:created relationships and the billingaddress entity

---
+++ src/main/java/developer/backend/CRUD/entity/BillingAddress.java
@@ -0,0 +1,33 @@
+package developer.backend.CRUD.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tb_billingaddress")
+public class BillingAddress {
+
+    @Id
+    @Column(name = "account_id")
+    private UUID id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "number")
+    private Integer number;
+}
