8b6843d7c756a3532f4e476a5cea6cd16eb40792
daniel2dfla <daniel_2dfla@hotmail.com>
2024-08-04 15:18:24 -0300

    feature: creating account entity

---
+++ src/main/java/developer/backend/CRUD/entity/Account.java
@@ -0,0 +1,26 @@
+package developer.backend.CRUD.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_accounts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @Column(name = "account_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID accountId;
+
+    @Column(name = "description")
+    private String Description;
+}
